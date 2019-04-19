cbc33f69f2f093d4250f3d4a7c29a038ab247b96
SirCQQ <48199024+SirCQQ@users.noreply.github.com>
2019-04-19 14:30:37 +0300

    Animatii
    Am adaugat animatii in index si package-ul pentru express

--- js/index.js
+++ js/index.js
@@ -1,9 +1,35 @@
-function register()
-{
-var login=document.getElementById("register");
-if(login.classList[0]==="hiddenregister"){
-login.classList.remove("hiddenregister");
-login.classList.add("notHiddenRegister");}
-else{login.classList.remove("notHiddenRegister");
-login.classList.add("hiddenregister");}
+function register() {
+    var register = document.getElementById("register");
+    if (register.classList[0] === "hidden") {
+        register.classList.remove("hidden");
+        register.classList.add("notHiddenRegister");
+    }else{
+    if (register.classList[0] === "hiddenRegister") {
+        register.classList.remove("hiddenRegister");
+        register.classList.add("notHiddenRegister");
+    }
+    else {
+        register.classList.remove("notHiddenRegister");
+        register.classList.add("hiddenRegister");
+    }
+}
+}
+
+function login() {
+    var login = document.getElementById("login");
+    if (login.classList[0] === "hidden") {
+        login.classList.remove("hidden");
+        login.classList.add("notHiddenLogin");
+    }
+    else{
+
+    if (login.classList[0] === "hiddenLogin") {
+        login.classList.remove("hiddenLogin");
+        login.classList.add("notHiddenLogin");
+    }
+    else {
+        login.classList.remove("notHiddenLogin");
+        login.classList.add("hiddenLogin");
+    }
+}
 }
