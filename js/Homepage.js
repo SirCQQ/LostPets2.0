2100776afa276a3923ad81f68d72518a7fb7c52b
SirCQQ <48199024+SirCQQ@users.noreply.github.com>
2019-05-07 16:30:43 +0300

    DA DA UPDATE CU GRID

--- js/Homepage.js
+++ js/Homepage.js
@@ -1,76 +1,122 @@
 function notificationFunction() {
-    var notification = document.getElementById("notifications");
+    console.log("clicked");
+    let notification = document.getElementById("notifications");
     if (notification.classList[0] === "hidden") {
         notification.classList.remove("hidden");
-        notification.classList.add("NotificationsShow");
-    }else{
-    if (notification.classList[0] === "NotificationNotShow") {
-        notification.classList.remove("NotificationNotShow");
-        notification.classList.add("NotificationsShow");
+        notification.classList.add("show-not");
+    } else {
+        if (notification.classList[0] === "hide-not") {
+            notification.classList.remove("hide-not");
+            notification.classList.add("show-not");
+        }
+        else {
+            notification.classList.remove("show-not");
+            notification.classList.add("hide-not");
+        }
+
+    }
+}
+
+function formularFunction() {
+    let formular = document.getElementById('input');
+    if (formular.classList[0] === "hidden") {
+        formular.classList.remove("hidden")
+        formular.classList.add("show-form")
     }
     else {
-        notification.classList.remove("NotificationsShow");
-        notification.classList.add("NotificationNotShow");
+        if (formular.classList[0] === "show-form") {
+            formular.classList.remove("show-form")
+            formular.classList.add("hide-form")
+        } else {
+            if (formular.classList[0] === "hide-form") {
+                formular.classList.remove("hide-form")
+                formular.classList.add("show-form")
+            }
+        }
     }
-
-}
 }
 
-
-
-let name="Max"
 function profileFunction() {
-    var profile = document.getElementById("profile");
-   
-    
-    
+    let profile = document.getElementById("profile");
     if (profile.classList[0] === "hidden") {
         profile.classList.remove("hidden");
-        profile.classList.add("ProfileShow");
-    }else{
-    if (profile.classList[0] === "NotProfileShow") {
-        profile.classList.remove("NotProfileShow");
-        profile.classList.add("ProfileShow");
+        console.log(profile.classList.length);
+        profile.classList.add("show-profile");
+    } else {
+        if (profile.classList[0] === "hide-profile") {
+            profile.classList.remove("hide-profile");
+            profile.classList.add("show-profile");
+        }
+        else {
+            profile.classList.remove("show-profile");
+            profile.classList.add("hide-profile");
+        }
     }
-    else {
-        profile.classList.remove("ProfileShow");
-        profile.classList.add("NotProfileShow");
-    }
-}  
 }
 
-function profile(){
-    var notification = document.getElementById("notifications");
-    if(notification.classList[0]==="NotificationsShow")
-    {
-        notification.classList.remove("NotificationsShow");
-        notification.classList.add("NotificationNotShow");
-        console.log("wait a sec");
-        setTimeout(profileFunction,1000);     
+
+
+
+function profile() {
+    let notification = document.getElementById("notifications");
+    let formular = document.getElementById('input');
+    if (notification.classList[0] === "show-not" || formular.classList[0] === "show-form") {
+        formular.classList.remove("show-form")
+        if (formular.classList.length === 0) {
+            formular.classList.add("hide-form")
+        }
+        notification.classList.remove("show-not");
+        if (notification.classList.length === 0) {
+            notification.classList.add("hide-not");
+        }
+        setTimeout(profileFunction, 500);
     }
     else {
         profileFunction();
     }
 }
-function notification(){
-    var profile = document.getElementById("profile");
-    console.log(profile);
-    console.log(profile.children[0].firstChild)
 
-    // console.log(profile.children[0].textContent)
-    if(profile.classList[0]==='ProfileShow')
-    {
-        profile.classList.remove("ProfileShow");
-        profile.classList.add("NotProfileShow");
-        setTimeout(notificationFunction,1005);
+
+
+
+function notification() {
+    let profile = document.getElementById("profile");
+    let formular = document.getElementById('input');
+    if (profile.classList[0] === 'show-profile' || formular.classList[0] === "show-form") {
+        formular.classList.remove("show-form")
+        if (formular.classList.length === 0) {
+            formular.classList.add("hide-form")
+        }
+        profile.classList.remove("show-profile");
+        if (profile.classList.length === 0){
+            profile.classList.add("hide-profile");
+        }
+        setTimeout(notificationFunction, 500);
     }
-    else{
+    else {
         notificationFunction();
     }
 }
 
-function change (){
-    var card= document.getElementById('info');
-    card.cardList.remove('infoNotShow')
-    card.cardList.add('infoShow')
+
+
+
+function formular() {
+    let profile = document.getElementById("profile");
+    let notification = document.getElementById("notifications");
+    if (profile.classList[0] === 'show-profile' || notification.classList[0] === "show-not") {
+        profile.classList.remove("show-profile");
+        if (profile.classList.length === 0) {
+            profile.classList.add("hide-profile");
+        }
+        notification.classList.remove("show-not");
+        if (notification.classList.length === 0) {
+            notification.classList.add("hide-not");
+        }
+        setTimeout(formularFunction, 500);
+    }
+    else {
+        formularFunction();
+    }
 }
+
